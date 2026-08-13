1e54a2a9772a183e73afe045ef0f32ee9246b25c
devopsencvr <musengakabongo@gmail.com>
2026-08-13 04:00:06 +0100

    Ajout du nettoyage des données Books to Scrape

--- scrapers/books_scraper.py
+++ scrapers/books_scraper.py
@@ -171,3 +171,66 @@ print("=" * 50)
 
 #Fermeture du navigateur
 driver1.quit()
+
+#Création du DataFrame pour sauvegarder les données brutes extraites de Source 1 — Books to Scrape
+df_livres = pd.DataFrame(data_Livres)
+
+df_livres_brutes=df_livres.copy()
+
+#NETTOYAGE DES DONNEES:
+
+#1.Nettoyage de la colonne Prix
+df_livres["Prix"]=df_livres["Prix"].str.replace("£", "", regex=False)
+
+#2.Nettoyage de la colonne Note
+df_livres["Note"] = df_livres["Note"].str.replace("star-rating ","",regex=False)
+conversion_notes = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5
+}
+df_livres["Note"] = df_livres["Note"].map(conversion_notes)
+
+#3.Conversion du nombre de reviews en valeur numérique
+df_livres["Nombre_reviews"] = pd.to_numeric(df_livres["Nombre_reviews"],errors="coerce")
+
+#4. Nettoyage de Tax
+df_livres["Tax"] = df_livres["Tax"].str.replace("£","",regex=False)
+
+#5. Nettoyage de la disponibilité
+df_livres["Disponibilité"] = df_livres["Disponibilité"].str.strip()
+
+#6. Nettoyage des variables texte
+df_livres["Titre"] = df_livres["Titre"].str.strip()
+df_livres["Description"] = df_livres["Description"].str.strip()
+df_livres["Categorie"] = df_livres["Categorie"].str.strip()
+
+#7. Vérification des valeurs manquantes
+print("\nValeurs manquantes par colonne :")
+print(df_livres.isnull().sum())
+
+
+#8. Vérification des doublons
+print("\nNombre de doublons :", df_livres.duplicated().sum())
+
+
+#9. Suppression des doublons
+df_livres = df_livres.drop_duplicates()
+
+
+#10. Vérification des types de données
+print("\nTypes des variables :")
+print(df_livres.dtypes)
+
+
+#11. Affichage des premières lignes nettoyées
+print("\nAperçu des données nettoyées :")
+print(df_livres.head())
+
+#Sauvegarde des données brutes
+df_livres_brutes.to_csv("data/raw/books_raw.csv",index=False,encoding="utf-8-sig")
+
+#Sauvegarde des données nettoyées
+df_livres.to_csv("data/cleaner/books_cleaned.csv",index=False,encoding="utf-8-sig")
